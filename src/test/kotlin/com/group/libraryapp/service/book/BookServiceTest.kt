b9fc6a1b4fcc2e70a772f2dbd2edd6cbbaa586d9
DonghwanSon1 <sdh549000@gmail.com>
2025-02-05 21:20:59 +0900

    feat: Book 엔티티의 type 추가

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -35,7 +35,7 @@ class BookServiceTest @Autowired constructor(
   @DisplayName("책 등록이 정상 동작한다.")
   fun saveBookTest() {
     // given
-    val request = BookRequest("스프링책")
+    val request = BookRequest("스프링책", "COMPUTER")
 
     // when
     bookService.saveBook(request)
@@ -44,13 +44,14 @@ class BookServiceTest @Autowired constructor(
     val books = bookRepository.findAll()
     assertThat(books).hasSize(1)
     assertThat(books[0].name).isEqualTo("스프링책")
+    assertThat(books[0].type).isEqualTo("COMPUTER")
   }
 
   @Test
   @DisplayName("책 대출이 정상 동작한다.")
   fun loanBookTest() {
     // given
-    bookRepository.save(Book("이상한 나라의 엘리스"))
+    bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
     val savedUser = userRepository.save(User(null,"손동환", null))
     val request = BookLoanRequest("손동환", "이상한 나라의 엘리스")
 
@@ -70,7 +71,7 @@ class BookServiceTest @Autowired constructor(
   @DisplayName("책이 진작 대출되어 있다면, 신규 대출이 실패한다.")
   fun loanBookFailTest() {
     // given
-    bookRepository.save(Book( "이상한 나라의 엘리스"))
+    bookRepository.save(Book.fixture("이상한 나라의 엘리스"))
     val savedUser = userRepository.save(User(null,"손동환", null))
     userLoanHistoryRepository.save(UserLoanHistory(null, savedUser, "이상한 나라의 엘리스", false))
     val request = BookLoanRequest(" 손동환", "이상한 나라의 엘리스")
