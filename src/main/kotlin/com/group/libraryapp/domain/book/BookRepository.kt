26a38d08f886c17777c4281373860b4bd6fd4f02
DonghwanSon1 <sdh549000@gmail.com>
2025-02-15 16:06:57 +0900

    fix : Book Repository 에서 JPQL -> QueryDSL 로 변경 및 리펙토링 ((인터페이스 + 구현체) 가 아닌 컴포넌트 QueryDSL 의 단일 구현체)

--- src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/book/BookRepository.kt
@@ -17,8 +17,8 @@ interface BookRepository: JpaRepository<Book, Long> {
    *
    *  따라서 QueryDSL + JPA 를 사용해보자
    */
-  @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) " +
-      "   FROM Book b" +
-      "   GROUP BY b.type ")
-  fun getStatus(): List<BookStatResponse>
+//  @Query("SELECT NEW com.group.libraryapp.dto.book.response.BookStatResponse(b.type, COUNT(b.id)) " +
+//      "   FROM Book b" +
+//      "   GROUP BY b.type ")
+//  fun getStatus(): List<BookStatResponse>
 }
